537d70bacb5daf45db509d4805f2e18c84b57288
Jiwoong Han <156386797+scorchedrice@users.noreply.github.com>
2024-08-22 15:09:12 +0900

    Update 집합.py

--- 한지웅/집합.py
+++ 한지웅/집합.py
@@ -1,35 +1,41 @@
 import sys
-# 시간초과.
-pivot_all = {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'}
-pivot = set()
+
+# import sys해서 받아야 하고 (시간초과)
+# deepcopy를 쓰면 메모리 초과
+
 N = int(input())
+
+lst = set()
+
+
 for _ in range(N):
-    data = input()
+    input = sys.stdin.readline().strip().split()
 
-    if data == 'all':
-        pivot = pivot_all
-
-    elif data == 'empty':
-        pivot = pivot.clear()
-
-    else:
-        new_data = data.split(' ')
-        order = new_data[0]
-        num = new_data[1]
-        check = num in pivot
-        if order == 'add' and check == False:
-            pivot.add(num)
-        elif order == 'remove' and check == True:
-            pivot.remove(num)
-        elif order == 'toggle' and check == True:
-            pivot.remove(num)
-        elif order == 'toggle' and check == False:
-            pivot.add(num)
-        elif order == 'check' and check == True:
-            print(1)
-        elif order == 'check' and check == False:
-            print(0)
+    if len(input) == 1:
+        if input[0] == 'all':
+            lst = {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'}
         else:
-            continue
-
-
+            lst.clear()
+    else:
+        order = input[0]
+        num = input[1]
+        if order == 'add':
+            # add
+            # if num not in lst:
+            lst.add(num)
+        elif order == 'check':
+            # check
+            if num in lst:
+                print('1')
+            else:
+                print('0')
+        elif order == 'remove':
+            # remove
+            if num in lst:
+                lst.remove(num)
+        else:
+            # toggle
+            if num in lst:
+                lst.remove(num)
+            else:
+                lst.add(num)
