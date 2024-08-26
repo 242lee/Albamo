9ce2828131c802eacd5530dc86aa6940af364d04
sangmu <dltkdandl@naver.com>
2024-08-26 17:41:47 +0900

    두번째 문제 풀이

---
+++ 이상무/240826Mon/집합.py
@@ -0,0 +1,100 @@
+'''
+문제
+비어있는 공집합 S가 주어졌을 때, 아래 연산을 수행하는 프로그램을 작성하시오.
+
+add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을 무시한다.
+remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을 무시한다.
+check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
+toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
+all: S를 {1, 2, ..., 20} 으로 바꾼다.
+empty: S를 공집합으로 바꾼다.
+입력
+첫째 줄에 수행해야 하는 연산의 수 M (1 ≤ M ≤ 3,000,000)이 주어진다.
+
+둘째 줄부터 M개의 줄에 수행해야 하는 연산이 한 줄에 하나씩 주어진다.
+
+출력
+check 연산이 주어질때마다, 결과를 출력한다.
+
+예제 입력 1 
+26
+add 1
+add 2
+check 1
+check 2
+check 3
+remove 2
+check 1
+check 2
+toggle 3
+check 1
+check 2
+check 3
+check 4
+all
+check 10
+check 20
+toggle 10
+remove 20
+check 10
+check 20
+empty
+check 1
+toggle 1
+check 1
+toggle 1
+check 1
+예제 출력 1 
+1
+1
+0
+1
+0
+1
+0
+1
+0
+1
+1
+0
+0
+0
+1
+0
+'''
+import sys
+
+m = int(sys.stdin.readline())
+calc = [0]*21
+
+for _ in range(m):
+    order = sys.stdin.readline().strip()
+
+    try:
+        command, num = order.split()
+        num = int(num)
+
+        if command == 'add': 
+            calc[num] = 1
+
+        elif command == 'remove':
+            calc[num] = 0
+
+        elif command == 'check':
+            if calc[num]==1:
+                print(1)
+            else: 
+                print(0)
+
+        elif command == 'toggle':
+            if calc[num] == 1:
+                calc[num] = 0
+            else:
+                calc[num] = 1
+
+    except:
+        if order == 'all':
+            calc = [1]*21
+
+        elif order == 'empty':
+            calc = [0]*21
