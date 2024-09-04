d3a421a51ecd1bedf95cf936bdde455ff4342fa7
Leehansol <gksthf996@naver.com>
2024-09-04 18:56:30 +0900

    한솔 로그프레소 실패

---
+++ 이한솔/20240909/로그프레소.py
@@ -0,0 +1,21 @@
+'''
+가장 먼저 감염된 컴퓨터 알아내기
+완전 탐색으로 풀어보자
+모르겠다
+'''
+
+# n, m, k = map(int, input().split())
+# infection = list(map(int, input().split()))
+
+# seq = []
+# log = []
+
+# for _ in range(n):
+#     t, a, b = map(int, input().split())
+#     seq.append(t)
+#     log.append((t,a,b))
+
+# seq.sort()
+
+# for s in seq:
+
