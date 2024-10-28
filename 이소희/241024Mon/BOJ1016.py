09b4c5f42d3c4e880f15197b7ee5231aab40598f
dlthgml1501 <dlthgml1501@gmail.com>
2024-10-28 15:46:32 +0900

    241028 스터디 문제

--- 이소희/241024Mon/BOJ1016.py
+++ 이소희/241024Mon/BOJ1016.py
@@ -4,11 +4,9 @@ is_square_free = [True] * (maxN - minN + 1)
 # 2부터 maxN의 제곱근까지의 수를 순회
 for i in range(2, int(maxN**0.5) + 1):
     square = i * i 
-    
-    # 제곱수의 배수를 minN 이상에서 찾기 위한 시작점 계산
+
     start = (minN + square - 1) // square * square
     
-    # 제곱수의 배수를 False로 마킹
     for j in range(start, maxN + 1, square):
         is_square_free[j - minN] = False
 
