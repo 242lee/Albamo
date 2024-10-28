dc789961a06f3dd1a985af558f8eea31eb2e5bcd
HoseaKim1025 <hoseakim1025@gmail.com>
2024-10-28 20:12:27 +0900

    호진 끈끈이주걱 품

--- Study/상무_샷건.py
+++
@@ -1,7 +0,0 @@
-keyboard = "".join(input() for _ in range(4))
-shotgun_points = input()
-
-key_positions = [keyboard.index(key) for key in shotgun_points]
-average_position = sum(key_positions) // 9
-
-print(keyboard[average_position])
---
+++ 김호진/Week10-2/호진_Coloring2.py
@@ -0,0 +1,4 @@
+# 백준 32381 Coloring 2: Electric Boogaloo 골2
+import sys
+input = sys.stdin.readline
+
